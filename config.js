c9346722aa39a2499d8c13f3f575fe166eac5e37
randomlim <lilitasgordis@gmail.com>
2026-02-10 22:05:51 -0300

    Update config.js

--- config.js
+++ config.js
@@ -78,10 +78,9 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
- ", // Music streaming URLhttps://youtu.be/Sl0-Y1cHFBI?si=2eUmo5J4t1alImP_
-        startText: "🎵 Play Music",        // Button text to start music
-        stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+ 
+           // Button text to start music
+          // Button text to stop musi                     // Volume level (0.0 to 1.0)
     }
 };
 
